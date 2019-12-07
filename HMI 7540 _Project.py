0be3bcd530922ab4d2f70fce123b57d4755f984b
nehaburse <54456728+nehaburse@users.noreply.github.com>
2019-12-06 19:32:56 -0500

    Update HMI 7540 _Project.py

--- HMI 7540 _Project.py
+++ HMI 7540 _Project.py
@@ -76,13 +76,17 @@ class Doctor(object):
 class Scheduler(object):
 
     def schedule(self,doctor,patient,time):
+        
         self.time = time
         self.doctor = doctor
         self.patient = patient
+        
         if self.isdoctoravailable(doctor,time) and self.ispatientavailable(patient,time):
             self.update_patient_calendar(patient,doctor,time)
             self.update_doctor_calendar(patient,doctor,time)
-            print ("Appointment Scheduled with Dr. " + doctor.doctor_name +  " for patient " + patient.patient_calendar[CLASS_NAME] + " at " + SLOT_11 + "\n")
+            
+            print ("Appointment Scheduled with Dr. " + doctor.doctor_name +  " for patient " + patient.patient_calendar[CLASS_NAME] + " at " + SLOT + "\n")
+        
             return True
 
     def isdoctoravailable(self,doctor,time):
@@ -105,6 +109,7 @@ class Scheduler(object):
     def update_doctor_calendar(self,patient,doctor,time):
         doctor.doctor_calendar[CLASS_APPOINTMENT_INFO][time] = patient.get_patient_record()
 
+        
 P01 = input("Enter Patient's first name: ")
 P02 = input("Enter Patient's last name: ")
 P03 = input("Enter Patient's ID: ")
@@ -112,15 +117,14 @@ P03 = input("Enter Patient's ID: ")
 D01 = input("Enter Doctor's first name: ")
 D02 = input("Enter Doctor's last name: ")
 D03 = input("Enter Doctor's Speciality: ")
-
-#SLOT = input("Enter Time: ")
+            
+SLOT = input("Enter Time: ")
 
 D1 = Doctor(D01, D02, D03)
 P1 = Patient(P01, P02, P03)
 
 S1 = Scheduler()
-S1.schedule(D1,P1,SLOT_11)
-#S1.schedule(D1,P1,SLOT)
+S1.schedule(D1,P1,SLOT)
 
 P1.print_patient_calendar()
 
